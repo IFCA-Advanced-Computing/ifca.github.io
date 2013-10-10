c1b36dd630aa6b5a6b0d6edb5c496d4e79b6c088
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2013-10-10 16:49:23 +0200

    Start using pelican for our web page.

---
+++ content/hello-world.md
@@ -0,0 +1,6 @@
+Title: Hello world
+Date: 2013-10-10 12:26
+Category: news
+Author: IFCA
+
+This is our very first post, stay tuned.
---
+++ pelicanconf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'IFCA'
+SITENAME = u'grid.ifca.es'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Madrid'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('Jinja2', 'http://jinja.pocoo.org/'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/IFCACluster'),)
+
+DEFAULT_PAGINATION = 10
+
+DISPLAY_CATEGORIES_ON_MENU = False
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+
+THEME = "themes/ifca"
+
+GITHUB_USER = "IFCA"
+GITHUB_REPO_COUNT = 20
+
+MENUITEMS = (('Home', '/'),
+             ('Blog', '/archives.html'),
+             ('Wiki', 'http://grid.ifca.es/wiki'),)
