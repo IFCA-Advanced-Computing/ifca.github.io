b0c380fc7a33736c053ae982c28185f8e83abe71
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2013-10-25 09:50:27 +0200

    Fixed categry feed

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,8 +11,8 @@ TIMEZONE = 'Europe/Madrid'
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
